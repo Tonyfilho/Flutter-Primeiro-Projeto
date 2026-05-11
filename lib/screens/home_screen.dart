4dee664baf6d7465aa117211fc419b5c14730649
Tonyfilho <oficinadosdrones@gmail.com>
2026-05-11 13:05:40 +0100

    update

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  UserCredential user;
+  // HomeScreen({super.key});
+
+ HomeScreen() {
+
+ }
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
